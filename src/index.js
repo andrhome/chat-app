a82d805a9eff7d995c5c293fc0b4cb19210371e3
Andrii.Khomenko <andrii.khomenko@xenial.com>
2022-01-30 19:21:10 +0200

    Created an express application instance, created socket.io connection and handling "connection" event.

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app)
+const io = socketio(server);
+const port = process.env.PORT || 3000;
+
+const publicDirPath = path.join(__dirname, '../public');
+
+// Setup static directory to serve
+app.use(express.static(publicDirPath));
+
+io.on('connection', () => {
+  console.log('New WebSocket connection!')
+});
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
